3bbf482f1de70d8c3471903c9c82234d1b3f866a
Diego Gonzalez <degonzalez6@gmail.com>
2022-02-23 03:14:02 -0300

    Commit 2: Se crea el componente UserTable.jsx Con este componente estamos armando una grilla donde visualizaremos usuarios, con botones de modificación. Se carga archivo CSS de ejemplo para dar estilo.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import UserTable from './components/UserTable';
+import { v4 as uuidv4 } from 'uuid';
+
 
 
 function App() {
+
+  const usersData=[
+    {id: uuidv4(), name: 'Tania', username: 'floppydiskette'},
+    {id: uuidv4(), name: 'Craig', username: 'siliconeidolon'},
+    {id: uuidv4(), name: 'Ben', username: 'benisphere'},
+  ]
+
+  //state
+  const [users, setUsers] = useState(usersData);
+
+
+
   return (
-    <div className="App">
-    
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className='flex-row'>
+        <div className='flex-large'>
+          <h2>Add user</h2>
+          
+        </div>
+        <div className='flex-large'>
+          <h2>View users</h2>
+          <UserTable users={users} />
+        </div>
+
+      </div>
     </div>
   );
 }
